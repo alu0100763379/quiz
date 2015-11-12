2324823ce2e38bd21f417ecae2e6d659de18c52a
alu0100763379 <alu0100@ull.edu.es>
2015-11-12 19:15:29 +0000

    Añadidas 7 preguntas

--- models/quiz_model.js
+++ models/quiz_model.js
@@ -9,8 +9,48 @@ function Quiz() {
         return (/^\s*roma\s*$/i).exec(x);
       }
     },
+    { pregunta: '¿Qué animal tiene en su nombre las cinco vocales?',
+      respuesta: function(x) {
+        return (/^\s*murci[eé]lago\s*$/i).exec(x);
+      }
+    },
+    { pregunta: 'Si me nombras desaparezco. ¿Qué soy?',
+      respuesta: function(x) {
+        return (/^\s*silencio\s*$/i).exec(x);
+      }
+    },
+    { pregunta: 'Si: 111 = 13, 112 = 24, 113 = 35, 114 = 46. Entonces 115 = ?',
+      respuesta: function(x) {
+        return (/^\s*57\s*$/i).exec(x);
+      }
+    },
+    { pregunta: 'No estoy vivo pero crezco. No tengo pulmones pero necesito el aire. No tengo boca pero el agua me mata. ¿Qué soy?',
+      respuesta: function(x) {
+        return (/^\s*fuego\s*$/i).exec(x);
+    }
+    },
+    { pregunta: '¿Cuántos años tiene un lustro?',
+      respuesta: function(x) {
+        return (/^\s*5\s*$/i).exec(x);
+    }
+    },
+    { pregunta: '¿Cómo se llama el lugar rodeado de tres cuerdas donde pelean los boxeadores?',
+      respuesta: function(x) {
+        return (/^\s*ring\s*$/i).exec(x);
+    }
+    },
+    { pregunta: '¿Cuál es el color que representa la esperanza?',
+      respuesta: function(x) {
+        return (/^\s*verde\s*$/i).exec(x);
+    }
+    },
+    { pregunta: '¿Qué cantidad de huesos tiene en total el ser humano?',
+      respuesta: function(x) {
+        return (/^\s*206\s*$/i).exec(x);
+    }
+    },
     {
-      pregunta: '¿Quien reinaba en España cuando se descubrió América?',
+      pregunta: '¿Quién reinaba en España cuando se descubrió América?',
       respuesta: function(x) {
         if ((/\b(Isabel\s+y?\s*Fernando)|(Fernando\s+[ey]?\s*Isabel)\b/i).exec(x)) {
           return true;
@@ -18,23 +58,6 @@ function Quiz() {
         if ((/\breyes\s+cat[oó]licos\b/i).exec(x)) { return true; }
         return false;
       },
-    },
-    { /* Código inseguro. ¡No ejecute esta pregunta salvo en un
-         entorno en el que el código del "alumno" sea fiable!
-       */
-      pregunta: 'Escriba una función JavaScript de nombre <tt>square</tt> '+
-      'que recibe un número y devuelve el cuadrado',
-      respuesta: function(x) {
-        try {
-          eval(x); /* DANGER DANGER DANGER */
-          var z = Math.floor(Math.random()*100);
-          return (square(z) == z*z);
-        }
-        catch(e) {
-          return false;
-        }
-        return false;
-      }
     }
   );
   // insertar unas cuantas preguntas sobre
